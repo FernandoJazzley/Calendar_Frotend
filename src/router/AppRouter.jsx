db9d5e31ada6f18183bb11ad5301385c99ba6852
FernandoJazzley <fernandojp2155@gmail.com>
2023-02-13 18:13:09 -0600

    cambios

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,7 +4,7 @@ import { CalendarPage } from "../calendar";
 
 export const AppRouter = () => {
 
-  const authStatus = 'authenticted'; //not-authenticted
+  const authStatus = 'not-authenticted'; //not-authenticted
 
   return (
     <Routes>
--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -4,16 +4,33 @@ export const authSlice = createSlice({
 
     name: 'auth',
     initialState: {
-        counter: 10
+        status: 'checking', //'authenticated', 'not-authenticated',
+        user: {},
+        errorMessage: undefined,
     },
     reducers: {
-        increment: (state, /* action */ ) => {
-            state.counter += 1;
+        onChecking: ( state ) => {
+            state.status = 'checking';
+            state.user = {};
+            state.errorMessage = undefined;
         },
+        onLogin: ( state, { payload } ) => {
+            state.status = 'authenticated';
+            state.user = payload;
+            state.errorMessage = undefined;
+        },
+        onLogout: ( state, { payload }) => {
+            state.status = 'not-authenticated';
+            state.user = {};
+            state.errorMessage = payload;
+        },
+        clearErrorMessage: ( state ) => {
+            state.errorMessage = undefined;
+        }
     }
 
 });
 
 // Action creators are generated for each case reducer function
 
-export const { increment } = authSlice.actions; 
+export const { onChecking, onLogin, onLogout, clearErrorMessage } = authSlice.actions; 
